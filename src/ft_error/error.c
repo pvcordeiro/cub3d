7fc9fc2f0dc62094c532da2d18121f9a3a6cce3b
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2025-04-28 18:10:23 +0100

    ReFACTOR

--- src/ft_error/error.c
+++ src/ft_error/error.c
@@ -6,7 +6,7 @@
 /*   By: afpachec <afpachec@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 22:00:34 by afpachec          #+#    #+#             */
-/*   Updated: 2025/04/28 18:02:47 by afpachec         ###   ########.fr       */
+/*   Updated: 2025/04/28 18:09:51 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,20 +36,20 @@ static ssize_t	ft_error_fputstr(int fd, char *string)
 	return (write(fd, string, len));
 }
 
-void	ft_error_assert(void)
+bool	ft_has_error(void)
 {
 	if (ft_error_storage()->error == ERROR_NO_ERROR)
+		return (false);
+	return (true);
+}
+
+void	ft_error_assert(void)
+{
+	if (!ft_has_error())
 		return ;
 	ft_error_fputstr(STDERR_FILENO, "Error\n");
 	ft_error_fputstr(STDERR_FILENO,
 		ft_error_message(ft_error_storage()->error));
 	ft_error_fputstr(STDERR_FILENO, "\n");
 	ft_error_storage()->exit(ft_error_storage()->error);
-}
-
-bool	ft_has_error(void)
-{
-	if (ft_error_storage()->error == ERROR_NO_ERROR)
-		return (false);
-	return (true);
 }
