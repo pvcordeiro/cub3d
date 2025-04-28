aed7f288de5ab06b67ed848acbabe2c9dc21f7a5
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2025-04-28 18:07:24 +0100

    Normie NET fix

--- src/ft_error/error.c
+++ src/ft_error/error.c
@@ -6,7 +6,7 @@
 /*   By: afpachec <afpachec@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 22:00:34 by afpachec          #+#    #+#             */
-/*   Updated: 2025/04/28 15:47:49 by afpachec         ###   ########.fr       */
+/*   Updated: 2025/04/28 18:02:47 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,8 @@ void	ft_error_assert(void)
 	if (ft_error_storage()->error == ERROR_NO_ERROR)
 		return ;
 	ft_error_fputstr(STDERR_FILENO, "Error\n");
-	ft_error_fputstr(STDERR_FILENO, ft_error_message(ft_error_storage()->error));
+	ft_error_fputstr(STDERR_FILENO,
+		ft_error_message(ft_error_storage()->error));
 	ft_error_fputstr(STDERR_FILENO, "\n");
 	ft_error_storage()->exit(ft_error_storage()->error);
 }
--- src/utils/map/map2.c
+++ src/utils/map/map2.c
@@ -1,49 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map.c                                              :+:      :+:    :+:   */
+/*   map2.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afpachec <afpachec@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 08:42:58 by afpachec          #+#    #+#             */
-/*   Updated: 2025/04/28 15:52:14 by afpachec         ###   ########.fr       */
+/*   Updated: 2025/04/28 18:06:34 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cub3d.h>
-
-static void	read_map_raw_lines_e(t_map *map)
-{
-	int		fd;
-	char	*line;
-	char	**tmp_line;
-	char	**tmp_map;
-
-	fd = open(map->path, O_RDONLY);
-	if (fd < 0)
-		return (ft_error(ERROR_MAP_OPEN));
-	while (1)
-	{
-		line = ft_get_next_line(fd);
-		if (!line)
-			break ;
-		if (ft_str_endswith(line, "\n"))
-			line[ft_strlen(line) - 1] = '\0';
-		tmp_map = map->raw;
-		tmp_line = ft_strvndup(&line, 1);
-		map->raw = ft_strvjoin(map->raw, tmp_line);
-		(ft_strvfree(tmp_map), ft_strvfree(tmp_line), free(line));
-	}
-	ft_close(fd);
-	if (!map->raw)
-		return (ft_error(ERROR_MAP_READ));
-	ft_error(ERROR_NO_ERROR);
-}
-
-static bool	is_map_char(char c)
-{
-	return (ft_isspace(c) || ft_strchr(MAP_CHARS, c));
-}
+#include "map.h"
 
 static void	add_variable_to_types_e(t_hashmap *types, char *line)
 {
@@ -72,7 +39,7 @@ static void	add_variable_to_types_e(t_hashmap *types, char *line)
 	ft_error(ERROR_NO_ERROR);
 }
 
-static void	process_raw_map_e(t_map *map)
+void	process_raw_map_e(t_map *map)
 {
 	size_t	i;
 
@@ -94,30 +61,4 @@ static void	process_raw_map_e(t_map *map)
 		map->map = ft_strvdup(&map->raw[i]);
 		break ;
 	}
-}
-
-void	parse_map_e(t_map *map, int argc, char **argv)
-{
-	if (map->initialized)
-		return (ft_error(ERROR_MAP_ALREADY_INITIALIZED));
-	if (argc != 2 || !argv || !argv[0] || !argv[1] || argv[2])
-		return (ft_error(ERROR_INVALID_ARGS));
-	map->path = argv[1];
-	if (!ft_str_endswith(map->path, ".cub"))
-		return (ft_error(ERROR_INVALID_MAP));
-	read_map_raw_lines_e(map);
-	if (ft_has_error())
-		return (ft_strvfree(map->raw), ft_bzero(map, sizeof(t_map)));
-	process_raw_map_e(map);
-	if (ft_has_error())
-		return (ft_strvfree(map->raw), ft_hashmap_destroy(map->types),
-			ft_bzero(map, sizeof(t_map)));
-	map->initialized = true;
-}
-
-void	destroy_map(t_map *map)
-{
-	ft_strvfree(map->raw);
-	ft_hashmap_destroy(map->types);
-	ft_bzero(map, sizeof(t_map));
 }
