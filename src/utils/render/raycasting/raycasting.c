c85bc62c8879a04ac95590808db8cce5e7d93271
Paulo Victor <pv.35@hotmail.com>
2025-05-03 22:59:11 +0100

    Refactor ray height calculation to include angle adjustment for improved rendering accuracy

--- src/utils/render/raycasting/raycasting.c
+++ src/utils/render/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 19:05:41 by paude-so          #+#    #+#             */
-/*   Updated: 2025/05/03 20:51:07 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/05/03 22:58:33 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	render_raycasting_mega(t_map *map, t_image *canvas)
 	int				ray_width;
 	int				i;
 	unsigned int	color;
+	double			angle_diff;
 
 	player = map->player->private;
 	ray_width = W_WIDTH / PLAYER_RAYS;
@@ -26,7 +27,9 @@ void	render_raycasting_mega(t_map *map, t_image *canvas)
 	i = -1;
 	while (++i < PLAYER_RAYS)
 	{
-		ray_size.height = W_HEIGHT / player->rays[i].length;
+		angle_diff = player->rays[i].angle - map->player->coords.yaw;
+		angle_diff = ft_normalize_angle(angle_diff) * (PI / 180.0);
+		ray_size.height = W_HEIGHT / (player->rays[i].length * cos(angle_diff));
 		color = 0x000000 + (0x111111 * (ray_size.height / (W_HEIGHT / 16)));
 		if (color > 0xFFFFFF)
 			color = 0xFFFFFF;
