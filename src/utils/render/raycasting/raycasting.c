2f6c7aea31a3d85a546a0b3dc741147de5291df8
Paulo Victor <pv.35@hotmail.com>
2025-05-03 23:52:27 +0100

    Refactor ray height calculation to ensure minimum length and limit maximum height for improved rendering accuracy

--- src/utils/render/raycasting/raycasting.c
+++ src/utils/render/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 19:05:41 by paude-so          #+#    #+#             */
-/*   Updated: 2025/05/03 22:58:33 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/05/03 23:51:49 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ void	render_raycasting_mega(t_map *map, t_image *canvas)
 	{
 		angle_diff = player->rays[i].angle - map->player->coords.yaw;
 		angle_diff = ft_normalize_angle(angle_diff) * (PI / 180.0);
-		ray_size.height = W_HEIGHT / (player->rays[i].length * cos(angle_diff));
+		ray_size.height = W_HEIGHT / (fmax(player->rays[i].length, 0.1) * cos(angle_diff));
+		ray_size.height = fmin(ray_size.height, W_HEIGHT * 3);
 		color = 0x000000 + (0x111111 * (ray_size.height / (W_HEIGHT / 16)));
 		if (color > 0xFFFFFF)
 			color = 0xFFFFFF;
