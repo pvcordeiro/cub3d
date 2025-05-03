1cb7e80fc2ed70fc607a31f1094076e5631f648e
Paulo Victor <pv.35@hotmail.com>
2025-05-04 00:54:59 +0100

    Refactor color calculation in raycasting to use gradient-based RGB values for improved rendering quality

--- src/utils/render/raycasting/raycasting.c
+++ src/utils/render/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 19:05:41 by paude-so          #+#    #+#             */
-/*   Updated: 2025/05/04 00:43:47 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/05/04 00:54:21 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@ void	render_raycasting_mega(t_map *map, t_image *canvas)
 	int				i;
 	unsigned int	color;
 	double			angle_diff;
+	double			gradient;
 
     draw_rectangle(canvas, (t_coords){0, 0, 0, 0}, 
-        (t_size){W_WIDTH, W_HEIGHT / 2}, 0x87CEEB, 0x87CEEB);
+        (t_size){W_WIDTH, W_HEIGHT / 2}, 0x4682B4, 0x4682B4);
     draw_rectangle(canvas, (t_coords){0, W_HEIGHT / 2, 0, 0}, 
-        (t_size){W_WIDTH, W_HEIGHT / 2}, 0x8B4513, 0x8B4513);
+        (t_size){W_WIDTH, W_HEIGHT / 2}, 0x5D4037, 0x5D4037);
 	player = map->player->private;
 	ray_width = W_WIDTH / PLAYER_RAYS;
 	ray_size.width = ray_width;
@@ -35,9 +36,11 @@ void	render_raycasting_mega(t_map *map, t_image *canvas)
 		angle_diff = ft_normalize_angle(angle_diff) * (PI / 180.0);
 		ray_size.height = W_HEIGHT / (fmax(player->rays[i].length, 0.1) * cos(angle_diff));
 		ray_size.height = fmin(ray_size.height, W_HEIGHT * 3);
-		color = 0x000000 + (0x111111 * (ray_size.height / (W_HEIGHT / 16)));
-		if (color > 0xFFFFFF)
-			color = 0xFFFFFF;
+		gradient = fmin(ray_size.height / (W_HEIGHT / 2.0), 1.0);
+        int r = (int)(gradient * 255);
+        int g = (int)(gradient * 255);
+        int b = (int)(gradient * 255);
+        color = (r << 16) | (g << 8) | b;
 		draw_rectangle(canvas, (t_coords){i * ray_width, (W_HEIGHT - ray_size.height) / 2, 0, 0}, ray_size, color, color); 
 	}
 }
