1be0621cddee2ad789a7faad3ec0a5072803b3be
Paulo Victor <pv.35@hotmail.com>
2025-06-04 18:03:53 +0100

    refactor action rendering for improved text positioning and clarity

--- src/utils/render/hud/action.c
+++ src/utils/render/hud/action.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   action.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afpachec <afpachec@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: paude-so <paude-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 02:27:23 by afpachec          #+#    #+#             */
-/*   Updated: 2025/06/03 01:25:30 by afpachec         ###   ########.fr       */
+/*   Updated: 2025/06/04 18:01:27 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ void	render_action(t_game *game, t_ftm_image *canvas)
 		return ;
 	text_config = (t_ftm_text_config){
 		.text = "Press SPACE to interact",
-		.coords = (t_coords){0, 0, 0},
-		.height = 15,
+		.coords = {0, 0, 0},
+		.height = canvas->size.height * 0.014,
 		.spacing = 0,
 		.color = 0xFFDDDDDD
 	};
 	text_config.coords.x = (canvas->size.width / 2)
 		- (ftm_get_text_size_prediction(game->hud.action.font, text_config) / 2);
-	text_config.coords.y = canvas->size.height - 200;
+	text_config.coords.y = canvas->size.height * 0.7;
 	ftm_draw_text_in_box(canvas, game->hud.action.font, text_config,
 		(t_ftm_rectangle){0x55000000, 0x55EFBF04, (t_size){2, 2}});
 }
